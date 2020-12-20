7ef3e117f9e9819d6af2e80020c532522cc47722
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-12-20 13:13:54 -0800

    Contacts Sidebar

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,3 +1,14 @@
+import { ListGroup } from 'react-bootstrap';
+import { useContacts } from '../contexts/ContactsProvider';
+
 export default function Contacts() {
-  return <div>Contacts</div>;
+  const { contacts } = useContacts();
+
+  return (
+    <ListGroup variant="flush">
+      {contacts.map((contact) => (
+        <ListGroup.Item ket={contact.id}>{contact.name}</ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 }
